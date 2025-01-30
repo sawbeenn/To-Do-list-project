d34563e741b8185def4163930873d9399ff39ff1
sawbeenn <sabindangol440@gmail.com>
2025-01-30 21:33:03 +0545

    final changes

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("List-container");
-
-const addTask = () => {
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
   if (inputBox.value === "") {
     alert("you must write something");
   } else {
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
-};
+  inputBox.value = "";
+  saveData();
+});
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
